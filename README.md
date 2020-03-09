9488e87aa4c57a0fd0f6e92d1cf6dee9aa7b57b0
Jeffrey Lin <jeffrey@icurse.nl>
2020-03-09 02:18:47 -0400

    database: Add initial script to fetch Yelp data

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Initialize the database:
 
 ```sh
 $ sqlite3 foodies.sqlite3 < database/initialize.sql
+$ JULIA_NUM_THREADS=4 database/get_data.jl
 ```
+
+Ideally, the database should be stored in memory as gathering data produces many
+write operations.
---
+++ database/get_data.jl
@@ -0,0 +1,129 @@
+#!/usr/bin/env julia
+
+# required environement variables: YELP_TOKEN
+
+module GetData
+
+using Base.Iterators: partition
+using JSON3
+using HTTP: post
+using SQLite: DB, Stmt
+using DBInterface: execute
+using Tables: rowtable
+
+const SQL_PRICE = "INSERT INTO prices (camis, source, price)
+                   VALUES (:camis, :source, :price)"
+
+const SQL_HOURS = "INSERT INTO openhours (camis, source, dayofweek, open, close)
+                   VALUES (:camis, :source, :dayofweek, :open, :close)"
+
+struct Exec
+    stmt::Stmt
+    args::NamedTuple
+end
+
+function db_write(ch)
+    while isopen(ch)
+        exe = take!(ch)
+        execute(exe.stmt, exe.args)
+    end
+end
+
+struct DBConnection
+    db::DB
+    price_stmt::Stmt
+    hours_stmt::Stmt
+    ch::Channel
+end
+
+function DBConnection(dbfile)
+    db = DB(dbfile)
+    DBConnection(db, Stmt(db, SQL_PRICE), Stmt(db, SQL_HOURS), Channel(db_write))
+end
+
+function get_missing_rows(dbc, source)
+    sql = Stmt(dbc.db,
+               "SELECT DISTINCT  r.camis, r.name, r.address, r.phone, r.lat, r.long
+                FROM             restaurants AS r
+                CROSS JOIN       sources     AS s
+                WHERE NOT EXISTS (SELECT 1 FROM prices    AS p
+                                  WHERE r.camis = p.camis AND p.source = :source)
+                  AND NOT EXISTS (SELECT 1 FROM openhours AS o
+                                  WHERE r.camis = o.camis AND o.source = :source)")
+
+    execute(sql, (source = source,))
+end
+
+insert_price(dbc, camis, price, source) =
+    put!(dbc.ch, Exec(dbc.price_stmt, (camis = camis, price = price, source = source)))
+
+function process_yelp_single(dbc, headers, row)
+    (camis, name, address, phone, lat, long) = row
+    addr, zipcode = rsplit(address; limit = 2)
+
+    res = post("https://api.yelp.com/v3/graphql", headers, """
+                {
+                    business_match(
+                        name: "$(escape_string(name))",
+                        address1: "$(escape_string(addr))",
+                        city: "New York",
+                        state: "NY",
+                        country: "US",
+                        $(lat isa Number ? "latitude: $lat," : "")
+                        $(long isa Number ? "longitude: $long," : "")
+                        phone: "$phone",
+                        postal_code: "$zipcode",
+                        limit: 1
+                    ) {
+                        total
+                        business {
+                            price
+                            hours {
+                                open {
+                                    day
+                                    start
+                                    end
+                                }
+                            }
+                        }
+                    }
+                }""")
+
+    data = JSON3.read(res.body).data.business_match
+    @info "YELP" name
+
+    if data.total == 1
+        b = data.business[1]
+        price = (b.price isa String ? length(b.price) : "NULL")
+        insert_price(dbc, camis, price, 2)
+
+        if length(b.hours) > 0
+            for (day, from, to) in b.hours[1].open
+                put!(dbc.ch, Exec(dbc.hours_stmt, (camis = camis, source = 2, dayofweek =
+                                                   day[2], open = from[2], close = to[2])))
+            end
+        end
+    end
+end
+
+function process_yelp(dbc)
+    missing_rows = get_missing_rows(dbc, 2) |> rowtable
+
+    headers = Dict("Authorization" => "Bearer $(ENV["YELP_TOKEN"])",
+                   "Content-Type" => "application/graphql")
+
+    for rows in partition(missing_rows, 4)
+        wait.(Threads.@spawn process_yelp_single(dbc, headers, row) for row in rows)
+        sleep(0.2) # avoid too many calls per second
+    end
+end
+
+end
+
+
+function main()
+    db = GetData.DBConnection("foodies.sqlite3")
+    GetData.process_yelp(db)
+end
+
+isinteractive() || main()
