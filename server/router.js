2513984cde6e0b4d2ba6748c5373c5d91e4f3ed1
Jeffrey Lin <jeffrey@icurse.nl>
2020-03-31 01:59:10 -0400

    server: add a route to get events by eid

--- server/router.js
+++ server/router.js
@@ -11,6 +11,8 @@ router.route("/users/:email").get(requireLogin, getUserByEmail);
 
 router.route("/categories").get(requireLogin, getBiasByUser);
 
+router.route("/events/:eid").get(requireLogin, getEventByEid);
+
 router.route("/users/login").post(requireLogin, logIn);
 
 export default router;
