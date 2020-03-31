eb29d7f0a6baf91c69033e70f8ef39cee731eee3
Jeffrey Lin <jeffrey@icurse.nl>
2020-03-31 00:51:22 -0400

    server: add a route to create events

--- server/db/events.js
+++ server/db/events.js
@@ -16,3 +16,45 @@ export async function getEvent(eid) {
     attendees,
   }));
 }
+
+const ROLES = Object.freeze({
+  host: 0,
+  guest: 1,
+});
+
+export async function insertEvent(uid, name, timestamp, budget, guests) {
+  const db = await getDbInstance();
+
+  const stmt = await db.run(
+    SQL`INSERT INTO events (name, timestamp, budget)
+        VALUES      (${name}, ${timestamp}, ${budget})`
+  );
+
+  const eventid = stmt.lastID;
+
+  const hProm = db.run(SQL`INSERT INTO attendees (uid, eid, role)
+                           VALUES      (${uid}, ${eventid}, ${ROLES.host})`);
+
+  const gProms = guests.map((email) =>
+    db.run(SQL`INSERT INTO attendees (uid, eid, role)
+               SELECT      uid, ${eventid}, ${ROLES.guest}
+               FROM        users
+               WHERE       email = ${email}`)
+  );
+
+  // get the emails that failed because they're not users
+  const invalid = new Set(guests);
+
+  await Promise.all([...gProms, hProm]);
+
+  await db.each(
+    `SELECT     email
+     FROM       users
+     INNER JOIN attendees USING(uid)
+     WHERE      eid = ${eventid}`,
+    [],
+    (_, { email }) => invalid.delete(email)
+  ); // TODO: how to handle err?
+
+  return { eid: eventid, invalid: Array.from(invalid) };
+}
