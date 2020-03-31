f2027a863ac5244425387bda814dc7850badef11
Jeffrey Lin <jeffrey@icurse.nl>
2020-03-31 01:59:10 -0400

    server: add a route to get events by eid

--- server/router.js
+++ server/router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createUser, getUserByEmail, logIn } from "./controllers/users";
 import { getBiasByUser } from "./controllers/categories";
+import { getEventByEid } from "./controllers/events";
 import { requireLogin } from "./services/passport";
 
 const router = Router();
@@ -10,6 +11,8 @@ router.route("/users").post(createUser);
 router.route("/users/:email").get(requireLogin, getUserByEmail);
 
 router.route("/categories").get(requireLogin, getBiasByUser);
+
+router.route("/events/:eid").get(requireLogin, getEventByEid);
 
 router.route("/users/login").post(requireLogin, logIn);
 
