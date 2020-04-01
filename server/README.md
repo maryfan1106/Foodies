40747f1a968e87c63d50ffc9b52a530d33885c03
Jeffrey Lin <jeffrey@icurse.nl>
2020-04-01 02:44:27 -0400

    server: add express.js boilerplate

---
+++ server/README.md
@@ -0,0 +1,16 @@
+# Server
+
+We use [Prettier](https://prettier.io/) for linting.
+Please run `npx prettier --write .` before committing.
+
+## Quick start
+
+Install dependencies and run in development mode:
+
+```sh
+$ npm install
+[...]
+$ npm run dev
+[...]
+Listening on port 3000
+```
---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import compression from "compression";
+import morgan from "morgan";
+import apiRouter from "./router";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(compression());
+app.use(express.json()); // replaces body-parser
+app.use(apiRouter);
+
+app.disable("x-powered-by");
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
