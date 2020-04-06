5fa9829e866a56bde9fa46b6086b67f889a04c11
Mary Fan <maryfan1106@gmail.com>
2020-04-06 04:03:24 -0400

    show attendees in event details

--- foodies_app/lib/models/eventdetail.dart
+++ foodies_app/lib/models/eventdetail.dart
@@ -1,24 +1,35 @@
 import 'package:meta/meta.dart' show required;
 
+import 'attendee.dart' show Attendee, AttendeeRole;
+
 class EventDetail {
   final int eid;
   final String name;
   final DateTime timestamp;
   final int budget;
+  final Attendee host;
+  final List<Attendee> guests;
 
   const EventDetail({
     @required this.eid,
     @required this.name,
     @required this.timestamp,
     @required this.budget,
+    @required this.host,
+    @required this.guests,
   });
 
   static EventDetail fromJson(dynamic parsedJson) {
+    final Iterable<Attendee> attending =
+        (parsedJson['attendees'] as List).map(Attendee.fromJson);
+
     return EventDetail(
       eid: parsedJson['eid'],
       name: parsedJson['name'],
       timestamp: DateTime.parse(parsedJson['timestamp']),
       budget: parsedJson['budget'],
+      host: attending.singleWhere((i) => i.role == AttendeeRole.host),
+      guests: attending.where((i) => i.role == AttendeeRole.guest).toList(),
     );
   }
 }
