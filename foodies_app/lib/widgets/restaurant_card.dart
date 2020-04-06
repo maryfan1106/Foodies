06b7bad10e1b9680545d273b67f389346a019f31
Mary Fan <maryfan1106@gmail.com>
2020-04-06 06:56:45 -0400

    keep track of selected restaurant for vote

--- foodies_app/lib/widgets/restaurant_card.dart
+++ foodies_app/lib/widgets/restaurant_card.dart
@@ -4,9 +4,13 @@ import '../models/restaurant.dart' show Restaurant;
 
 class RestaurantCard extends StatefulWidget {
   final Restaurant restaurant;
+  final bool selected;
+  final Function setVote;
 
   const RestaurantCard({
     @required this.restaurant,
+    @required this.selected,
+    @required this.setVote,
   });
 
   @override
@@ -16,34 +20,40 @@ class RestaurantCard extends StatefulWidget {
 class _RestaurantCardState extends State<RestaurantCard> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(10, 0, 0, 10),
-      child: Column(
-        children: <Widget>[
-          Center(
-            child: Text(
-              widget.restaurant.name,
-              style: const TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 16.0,
+    return InkWell(
+      onTap: () => widget.setVote(widget.restaurant),
+      child: Card(
+        color: widget.selected ? Colors.lightBlueAccent : null,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 0, 0, 10),
+          child: Column(
+            children: <Widget>[
+              Center(
+                child: Text(
+                  widget.restaurant.name,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16.0,
+                  ),
+                ),
               ),
-            ),
+              const Padding(padding: EdgeInsets.symmetric(vertical: 5.0)),
+              Center(
+                child: Text(
+                  widget.restaurant.address,
+                  style: const TextStyle(fontSize: 12.0),
+                ),
+              ),
+              const Padding(padding: EdgeInsets.symmetric(vertical: 5.0)),
+              Center(
+                child: Text(
+                  widget.restaurant.phone.toString(),
+                  style: const TextStyle(fontSize: 12.0),
+                ),
+              ),
+            ],
           ),
-          const Padding(padding: EdgeInsets.symmetric(vertical: 5.0)),
-          Center(
-            child: Text(
-              widget.restaurant.address,
-              style: const TextStyle(fontSize: 12.0),
-            ),
-          ),
-          const Padding(padding: EdgeInsets.symmetric(vertical: 5.0)),
-          Center(
-            child: Text(
-              widget.restaurant.phone.toString(),
-              style: const TextStyle(fontSize: 12.0),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
