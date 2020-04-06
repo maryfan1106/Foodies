cb4bc4e02597087c0e1f3b2aefa781b17358b0a4
Jeffrey Lin <jeffrey@icurse.nl>
2020-04-05 20:34:26 -0400

    server: add a route to get user vote for an event

--- server/controllers/votes.js
+++ server/controllers/votes.js
@@ -1,8 +1,16 @@
-import { insertVote } from "../db";
+import { getVote, insertVote } from "../db";
 
 export function voteEvent(req, res) {
   const { camis } = req.body;
   insertVote(req.user.uid, req.params.eid, camis)
     .then((vote) => res.status(201).json(vote))
     .catch((err) => res.status(400).json({ err }));
+}
+
+export function getVoteByEid(req, res) {
+  getVote(req.user.uid, req.params.eid).then((vote) =>
+    vote
+      ? res.status(302).json(vote)
+      : res.status(404).json({ err: "Vote not found" })
+  );
 }
