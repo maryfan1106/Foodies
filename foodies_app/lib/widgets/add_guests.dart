689accd3a61226688dc1d18a22c5ca4cbea33e31
Mary Fan <maryfan1106@gmail.com>
2020-04-06 06:56:45 -0400

    app: add attendees widget to create event page
    Reuse the attendee model, and set default role to "guest" since we won't
    need a role for this purpose.

---
+++ foodies_app/lib/widgets/add_guests.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+import '../models/attendee.dart' show Attendee;
+import '../services/users.dart' show getUser;
+
+class AddGuests extends StatefulWidget {
+  final List<Attendee> guests;
+
+  const AddGuests({
+    @required Key key,
+    @required this.guests,
+  }) : super(key: key);
+
+  @override
+  AddGuestsState createState() => AddGuestsState();
+}
+
+class AddGuestsState extends State<AddGuests> {
+  final _emailField = TextField(
+    controller: TextEditingController(),
+    obscureText: false,
+    decoration: const InputDecoration(
+      contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+      hintText: 'Attendee Email',
+    ),
+  );
+
+  void _addNewGuest(Attendee guest) => setState(() => widget.guests.add(guest));
+
+  @override
+  void dispose() {
+    _emailField.controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final addButton = RaisedButton(
+      child: const Text('Add Attendee'),
+      onPressed: () async {
+        Attendee result = await getUser(_emailField.controller.text);
+        _addNewGuest(result);
+        _emailField.controller.clear();
+      },
+    );
+
+    return Column(
+      children: <Widget>[
+        ListView.builder(
+          shrinkWrap: true,
+          itemCount: widget.guests.length,
+          itemBuilder: (context, index) {
+            final Attendee guest = widget.guests[index];
+            return Card(
+              child: ListTile(
+                title: Text(guest.email),
+              ),
+            );
+          },
+        ),
+        const SizedBox(height: 15.0),
+        _emailField,
+        const SizedBox(height: 25.0),
+        addButton,
+      ],
+    );
+  }
+}
