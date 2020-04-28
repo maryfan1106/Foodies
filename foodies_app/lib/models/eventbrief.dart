82f23801149901388e980fa5e7088bf7a227e727
Mary Fan <maryfan1106@gmail.com>
2020-04-27 23:39:40 -0400

    add event model and events widget

---
+++ foodies_app/lib/models/eventbrief.dart
@@ -0,0 +1,27 @@
+import 'package:meta/meta.dart' show required;
+
+class EventBrief {
+  final int eid;
+  final String name;
+  final DateTime timestamp;
+  final int budget;
+  final int attendees;
+
+  const EventBrief({
+    @required this.eid,
+    @required this.name,
+    @required this.timestamp,
+    @required this.budget,
+    @required this.attendees,
+  });
+
+  static EventBrief fromJson(dynamic parsedJson) {
+    return EventBrief(
+      eid: parsedJson['eid'],
+      name: parsedJson['name'],
+      timestamp: DateTime.parse(parsedJson['timestamp']),
+      budget: parsedJson['budget'],
+      attendees: parsedJson['attendees'],
+    );
+  }
+}
