4bac029a15e1a589498fac1a8f6de6987114d9bf
Mary Fan <maryfan1106@gmail.com>
2020-04-29 11:26:40 -0400

    app: send vote to server

--- foodies_app/lib/services/votes.dart
+++ foodies_app/lib/services/votes.dart
@@ -1,7 +1,22 @@
-import 'foodiesapi.dart' show FoodiesData, foodiesGet;
+import 'dart:io' show HttpStatus;
+
+import 'foodiesapi.dart' show FoodiesData, foodiesGet, foodiesPost;
 
 Future<bool> getVote(int eid) async {
   final FoodiesData fdata = await foodiesGet('/events/$eid/vote');
   // TODO: better error handling
   return fdata.body['camis'] != null;
+}
+
+Future<bool> voteForRestaurant(int eid, int camis) async {
+  final FoodiesData fdata =
+      await foodiesPost('/events/$eid/vote', data: {'camis': camis});
+
+  switch (fdata.status) {
+    case HttpStatus.created:
+      return true;
+      break;
+  }
+
+  return false;
 }
