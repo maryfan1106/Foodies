244ad108c63a4df5c392ec9d95efa4f81826b66d
Mary Fan <maryfan1106@gmail.com>
2020-05-03 17:02:33 -0400

    show numAttending, price, formatted time in events

--- foodies_app/lib/widgets/events.dart
+++ foodies_app/lib/widgets/events.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../models/eventbrief.dart' show EventBrief;
 import '../pages/event_screen.dart' show EventScreen;
+import '../services/locale.dart' show formatTimestamp;
 
 class Events extends StatelessWidget {
   final List<EventBrief> events;
@@ -9,6 +10,8 @@ class Events extends StatelessWidget {
   const Events({
     @required this.events,
   });
+
+  static const Padding _padding = Padding(padding: EdgeInsets.all(5.0));
 
   @override
   Widget build(BuildContext context) {
@@ -25,9 +28,43 @@ class Events extends StatelessWidget {
             ),
           ),
           child: Card(
-            child: ListTile(
-              title: Center(child: Text(event.name)),
-              subtitle: Center(child: Text(event.timestamp.toString())),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(10, 0, 0, 10),
+              child: Column(
+                children: <Widget>[
+                  _padding,
+                  Center(
+                    child: Text(
+                      event.name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                  _padding,
+                  Center(
+                    child: Text(
+                      '${event.numAttending} guests attending',
+                      style: const TextStyle(fontSize: 12.0),
+                    ),
+                  ),
+                  _padding,
+                  Center(
+                    child: Text(
+                      formatTimestamp(event.timestamp),
+                      style: const TextStyle(fontSize: 12.0),
+                    ),
+                  ),
+                  _padding,
+                  Center(
+                    child: Text(
+                      '\$' * event.budget,
+                      style: const TextStyle(fontSize: 12.0),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         );
