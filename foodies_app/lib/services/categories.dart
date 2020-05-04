c8a13b2caa356db7afba6fc0d1ec2450e3d1b1f9
Mary Fan <maryfan1106@gmail.com>
2020-05-04 00:19:53 -0400

    show biases on profile page

---
+++ foodies_app/lib/services/categories.dart
@@ -0,0 +1,17 @@
+import 'dart:io' show HttpStatus;
+
+import '../models/category.dart' show Category;
+import 'foodiesapi.dart' show FoodiesData, foodiesGet;
+
+Future<List<Category>> getCategories() async {
+  FoodiesData fdata = await foodiesGet('/categories');
+
+  switch (fdata.status) {
+    case HttpStatus.found:
+      List<Category> categories =
+          fdata.body.map<Category>(Category.fromJson).toList();
+      return categories;
+  }
+
+  return [];
+}
