e3a88acc06cec4f01f83ef2f3b3075fcacb3e234
Mary Fan <maryfan1106@gmail.com>
2020-05-04 00:19:53 -0400

    show biases on profile page

--- foodies_app/lib/pages/profile_screen.dart
+++ foodies_app/lib/pages/profile_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../models/category.dart' show Category;
+import '../services/categories.dart' show getCategories;
+import '../widgets/biases_display.dart' show BiasesDisplay;
+
 class ProfileScreen extends StatefulWidget {
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
@@ -8,18 +12,31 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.more_horiz),
-            iconSize: 30.0,
-            color: Colors.white,
-            onPressed: () {},
+    return FutureBuilder(
+      future: getCategories(),
+      builder: (BuildContext context, AsyncSnapshot<List<Category>> snapshot) {
+        Widget body;
+        if (snapshot.hasData) {
+          body = BiasesDisplay(categories: snapshot.data);
+        } else {
+          body = const Center(child: CircularProgressIndicator());
+        }
+
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Profile'),
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.more_horiz),
+                iconSize: 30.0,
+                color: Colors.white,
+                onPressed: () {},
+              ),
+            ],
           ),
-        ],
-      ),
+          body: body,
+        );
+      },
     );
   }
 }
