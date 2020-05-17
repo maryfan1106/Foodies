bdc0ed0bc0b9bcde6597e0a1b7c10651886f1f46
Mary Fan <maryfan1106@gmail.com>
2020-05-17 00:04:20 -0400

    app: fixed no user handler

--- foodies_app/lib/services/users.dart
+++ foodies_app/lib/services/users.dart
@@ -3,13 +3,15 @@ import 'dart:io' show HttpStatus;
 import '../models/attendee.dart' show Attendee;
 import 'foodiesapi.dart' show FoodiesData, foodiesGet;
 
-Future<Attendee> getUser(String email) async {
+Future<Attendee> getUser(String email, Function(String) err) async {
   final FoodiesData fdata = await foodiesGet('/users/$email');
   switch (fdata.status) {
     case HttpStatus.found:
       final Attendee user = Attendee.fromJson(fdata.body);
+      err(null);
       return user;
   }
 
-  throw Exception("No user found");
+  err(fdata.body['error']);
+  return null;
 }
